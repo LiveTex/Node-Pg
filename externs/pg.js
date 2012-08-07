087eeb0942ce6315dfed9da8765a463dea5d3785
Sergey Kononenko <kononencheg@gmail.com>
2012-08-07 17:43:58 +0400

    externs interface

---
+++ externs/pg.js
@@ -0,0 +1,24 @@
+
+
+
+/**
+ * @namespace
+ */
+var pg = {};
+
+
+/**
+ * @param {number} size
+ * @param {!Object} options
+ */
+pg.init = function(size, options) {};
+
+
+/**
+ * @param {string} query
+ * @param {function(Error, Array.<!Object.<string, string>>)} callback
+ */
+pg.exec = function(query, callback) {};
+
+
+pg.destroy = function() {};
